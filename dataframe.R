a69875cc4d7feeddaef4f4efbcebd82c701190b7
lsahni82 <lalit.sahni@gmail.com>
2017-08-21 16:13:31 +0530

    21.08.2017

---
+++ dataframe.R
@@ -0,0 +1,20 @@
+mtcars[1]
+colnames(mtcars)=c("crb","carbon","")mtcars
+mtcars
+colnames(mtcars)[3]
+dim(mtcars)[2]
+length(mtcars)
+attach(mtcars)
+df
+detach(mtcars)
+attach(mtcars)
+mpg
+df=mtcars
+mean(mpg)
+mean(cyl)
+df$carb = NULL
+str(df)
+sort(mtcars)
+sort(mtcars[1,])
+df$gear2=df$gear;
+sort(mtcars$mpg,decreasing=T)
