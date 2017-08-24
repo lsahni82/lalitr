ae3974cc05df3a1d33944c5e5ea1cf42f57add68
lsahni82 <lalit.sahni@gmail.com>
2017-08-24 19:01:27 +0530

    24.08/2017

---
+++ test.R
@@ -0,0 +1,10 @@
+a=c(1,2,3)
+a
+
+marks=c(10 ,10, 10, 20, 20 ,20, 20, 20, 30 ,30, 10, 10, 40, 40, 40 ,40, 50, 50 ,50, 50)
+marks
+table(marks)
+cbind(marks)
+cbind(table(marks))
+hist(marks)
+barplot(marks)
