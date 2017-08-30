338920ae39cf9d09e27fbf535041ff6d02041f17
Lalit Sahni <lalit.sahni@gmail.com>
2017-08-30 22:36:25 +0530

    scan

---
+++ scan.R
@@ -0,0 +1,22 @@
+# takind data from excel/keybord
+
+x=scan()
+1	43
+2	60
+3	37
+4	48
+5	65
+6	48
+7	57
+8	78
+9	31
+10	59
+11	50
+x
+x=scan()
+x
+y=scan()# error for character copying
+?scan
+y=scan(what='character')
+median(x)
+
